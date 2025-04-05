71059cc2bb03c33c37e273482c0cf2c0a29c0f43
Val <valeryl@g.clemson.edu>
2025-04-05 11:06:25 -0400

    update passages

--- embeddings.py
+++ embeddings.py
@@ -3,7 +3,7 @@ from sentence_transformers import SentenceTransformer
 from datasets import load_dataset
 import numpy as np
 
-model = SentenceTransformer("sentence-transformers/all_MiniLM-L6-v2")
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 msmarco_train = load_dataset("ms_marco", "v2.1", split="train")
 os.makedirs("embeddings", exist_ok=True)
 
@@ -12,8 +12,11 @@ all_passages = []
 
 for example in msmarco_train:
     queries.append(example["query"])
-    for p in example["passages"]:
-        all_passages.append(p["passage_text"])
+
+    for i, selected in enumerate(example["passages"]["is_selected"]):
+        if selected == 1:
+            text = example["passages"]["passage_text"][i]
+            all_passages.append(text)
 
 print("embedding passages")
 passage_embeddings = model.encode(all_passages, batch_size=128, show_progress_bar=True)
