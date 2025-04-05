c0004e7723b4d5026d9fcd46fd9e4f0044038b12
Val <valeryl@g.clemson.edu>
2025-04-05 16:03:05 -0400

    dubuig

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -28,6 +28,11 @@ def k_means(doc_vectors, n_clusters, flag_spherical=True):
     :param flag_spherical: Flag that indicates whether to run Spherical (True) or Standard (False) Kmeans.
     :return: The centroid of each cluster and for each document it returns the corresponding cluster.
     """
+    d = doc_vectors.shape[1]
+
+    # Debug: print overall doc shape
+    print(f"[INFO] Full doc_vectors shape: {doc_vectors.shape}")
+    print(f"[INFO] Embedding dimension: {d}")
 
     clustering = faiss.Kmeans(doc_vectors.shape[1],
                               n_clusters,
@@ -36,7 +41,7 @@ def k_means(doc_vectors, n_clusters, flag_spherical=True):
 
     samples = np.random.choice(doc_vectors.shape[0], min(39 * n_clusters, doc_vectors.shape[0]), replace=False)
     clustering.train(doc_vectors[samples])
-
+    print(f"[INFO] Searching cluster labels for all {doc_vectors.shape[0]} documents...")
     _, label_clustering = clustering.index.search(doc_vectors, 1)
 
     return clustering.centroids, label_clustering
